a04a24607ff4f8baee290b3a60ef573a3298c6dd
Honza Pospisil <me@honzapospisil.com>
2022-09-06 12:36:31 +0000

    Split test init

--- tests/init.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-include_once __DIR__ . '/load.php';
-include_once __DIR__ . '/functionas.php';
-
-Tester\Environment::setup();
--- tests/setup.php
+++ tests/setup.php
@@ -6,7 +6,7 @@ use Nette\Utils\Finder;
 
 require __DIR__ . '/../vendor/autoload.php';
 
-$runner->addPhpIniOption('auto_prepend_file', __DIR__ . '/init.php');
+$runner->addPhpIniOption('auto_prepend_file', __DIR__ . '/boot.php');
 foreach (Finder::findFiles('*.test.php')->from(__DIR__ . '/../src') as $key => $file) {
     $runner->paths[] = $key;
 }
